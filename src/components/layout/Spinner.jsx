5e6246684e9d1abc71340515abe26289aad5c40d
Erin Winking <erin@douglasavenue.com>
2022-04-25 22:35:08 -0500

    Completed lesson 51, added spinner no one sees.

---
+++ src/components/layout/Spinner.jsx
@@ -0,0 +1,16 @@
+import SpinnerGIF from './assets/spinner.gif'
+
+function Spinner() {
+	return (
+		<div className='w-100 mt-20'>
+			<img
+				width={180}
+				className='text-center mx-auto'
+				src={SpinnerGIF}
+				alt='loading'
+			/>
+		</div>
+	)
+}
+
+export default Spinner
